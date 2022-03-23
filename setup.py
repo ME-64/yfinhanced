2de2c7c576fb068a4697c9e48ddc2509eee9444f
ME-64 <miloelliott64@gmail.com>
2022-03-23 17:59:03 +0000

    github workflow setup

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         keywords = ['api wrapper', 'yahoo finance', 'markets'],
         include_package_data = True,
         zip_safe = False,
-        install_requires=['pandas', 'aiohttp', 'asyncio', 'requests', 'pytz', 'requests'],
+        install_requires=['pandas', 'aiohttp', 'asyncio', 'pytz', 'requests'],
         extras_require={
             "dev": ['pytest', 'pyteset-asyncio']},
         classifiers=[
